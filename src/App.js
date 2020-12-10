49ba53333b708add3bf9df5b1a9d6f6732514dde
root <root@DESKTOP-AVS43LE.localdomain>
2020-12-10 18:32:27 +0600

    Added some styles in App.css and testing App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import Circle from './Circles/Circles';
 
 class App extends Component {
   state={
-
+    numbers:[
+      {circle1:5},
+      {circle2:11},
+      {circle3:16},
+      {circle4:23},
+      {circle5:32}
+    ]
   }
   render() {
     return (
       <div className="App">
-        <Circle/>
+        <Circle circle1={this.state.numbers[0].circle1}/>
+        <Circle circle1={this.state.numbers[1].circle2}/>
+        <Circle circle1={this.state.numbers[2].circle3}/>
+        <Circle circle1={this.state.numbers[3].circle4}/>
+        <Circle circle1={this.state.numbers[4].circle5}/>
       </div>
     );
   }
--- src/Circles/Circles.js
+++ src/Circles/Circles.js
@@ -1,9 +1,13 @@
-// import React from 'react';
+import React from 'react';
 
 const Circle = props =>{
     return(
         <div className='mainBlock'>
-            
+            <p className='circle' id ='circle1'>{props.circle1}</p>
+            <p className='circle' id ='circle2'>{props.circle2}</p>
+            <p className='circle' id ='circle3'>{props.circle3}</p>
+            <p className='circle' id ='circle4'>{props.circle4}</p>
+            <p className='circle' id ='circle5'>{props.circle5}</p>
         </div>
     )
 };
